7ffec0fc12c6e90b9e419e26da7e377ed431a7b8
cavernxxx@gmail.com <cavernxxx@gmail.com@5a83989a-60d1-11de-a6a3-193033c112f2>
2009-07-08 12:24:35 +0000

    Removed some qDebug() Removed useless signals passing from BattleChannel to Users. Now they are connected directly. Replaced bit fields with classes, that extract and pack values using bit operations.
    git-svn-id: https://qtlobby.googlecode.com/svn/trunk@100 5a83989a-60d1-11de-a6a3-193033c112f2

--- src/AudioBackend.cpp
+++ src/AudioBackend.cpp
@@ -12,19 +12,12 @@
 #include "AudioBackend.h"
 
 AudioBackend::AudioBackend( QObject *parent ) : QObject( parent ) {
-#if QT_VERSION >= 0x040400
-  qDebug() << "PhononBackend loaded";
-#endif
+
 }
 
 AudioBackend::~AudioBackend() { }
 
 void AudioBackend::playSample( SampleCollection s ) {
   qDebug() << __FUNCTION__ << ": playing a sample: " << s;
-#if QT_VERSION >= 0x040400
-  qDebug() << "phonon called made";
- // phononBackend.play();
-#else
   qDebug() << "can't play sample, PhononBackend not found. Maybe your version of QT is < 4.4.x";
-#endif
 }
