932132a2caaef96ef99b1b7555d0a08e63a54f14
mirko.windhoff <mirko.windhoff@5a83989a-60d1-11de-a6a3-193033c112f2>
2009-07-15 00:07:54 +0000

    git-svn-id: https://qtlobby.googlecode.com/svn/trunk@180 5a83989a-60d1-11de-a6a3-193033c112f2

---
+++ qtlobby/src/main.cpp
@@ -0,0 +1,10 @@
+#include <QCoreApplication>
+#include "qtlobby.h"
+
+
+int main(int argc, char** argv)
+{
+    QCoreApplication app(argc, argv);
+    qtlobby foo;
+    return app.exec();
+}
---
+++ qtlobby/src/qtlobby.cpp
@@ -0,0 +1,9 @@
+#include "qtlobby.h"
+
+qtlobby::qtlobby()
+{}
+
+qtlobby::~qtlobby()
+{}
+
+#include "qtlobby.moc"
