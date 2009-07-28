78beaa9c715de1e2d027fdbed2d0bbfcd977d3ec
marcel.hauf <marcel.hauf@5a83989a-60d1-11de-a6a3-193033c112f2>
2009-07-28 15:12:59 +0000

    Readd inline functions in AbstractNotificationBackend to fix compiler errors.
    git-svn-id: https://qtlobby.googlecode.com/svn/trunk@257 5a83989a-60d1-11de-a6a3-193033c112f2

--- src/AbstractNotificationBackend.h
+++ src/AbstractNotificationBackend.h
@@ -11,21 +11,24 @@ public:
     QString applicationName;
 
     // Uses the application name for the title
-    virtual void showMessage(QString& message)
-    {
-        if(applicationName == NULL)
-        {
-            QString app_name = QString("Application");
-            showMessage(app_name, message, -2);
-        }
-        else
-        {
-            showMessage(applicationName, message, -2);
-        }
-    }
+    virtual void showMessage(QString& message);
     virtual void showMessage(QString& title, QString& message, int timeout_ms = -2);
     // timeout in milliseconds, 0 displays the message endless, -1 is system based, -2 is message length based
     virtual void showMessage(QString& title, QString& message, QString& icon_path, int timeout_ms = -2);
     // TODO: display and hide window with actions
 };
+inline void AbstractNotificationBackend::showMessage(QString& message)
+{
+    if(applicationName == NULL)
+    {
+        QString app_name = QString("Application");
+        showMessage(app_name, message, -2);
+    }
+    else
+    {
+        showMessage(applicationName, message, -2);
+    }
+}
+inline void AbstractNotificationBackend::showMessage(QString& title, QString& message, int timeout_ms){}
+inline void AbstractNotificationBackend::showMessage(QString& title, QString& message, QString& icon_path, int timeout_ms) {}
 #endif // ABSTRACTNOTIFICATIONBACKEND_H
